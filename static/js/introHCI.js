1d1bf04fc18da9c2eb4a66b7ef206a84af41f0e6
KawaiiPotato3526 <yas089@ucsd.edu>
2020-01-22 22:21:16 -0800

    before nodejs

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,32 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Yes, it is indeed working");
+     $('.jumbotron p').text("I am changing with h1 sista");
+    $('.jumbotron p').addClass("active");
+    // $('.jumbotron p').toggleClass("active");
+
+    $("#testjs").text("Please wait...")
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+  $("a.thumbnail").click(projectClick)
 }
+
+function projectClick(e){
+    e.preventDefault();
+    $(this).css("background-color","pink")
+
+   var containingProject = $(this).closest(".project");
+   var description =$(containingProject).find(".project-description");
+   if(description.length == 0){
+    $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+   }else{
+    // description.html("<p>You clicked me at " + (new Date()) + " I'm gonna call the police</p>");
+    description.fadeOut();
+   }
+  }
+
+
